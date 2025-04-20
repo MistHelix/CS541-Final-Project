1f45cec42ec6760fc13ca87728ab0a37217dee4c
Paola Frunzio <pfrunzio@wpi.edu>
2025-04-20 12:27:17 -0400

    setting things up

---
+++ README.txt
@@ -0,0 +1,2 @@
+# CS541-Final-Project
+Weather Prediction with Deep Learning
---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+    df = pd.read_csv("Dataset/3997445.csv")
+    print(df)
+
+
+
+if __name__ == '__main__':
+    main()
